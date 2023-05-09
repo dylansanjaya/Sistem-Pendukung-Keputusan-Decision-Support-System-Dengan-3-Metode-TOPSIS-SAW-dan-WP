a02794c22a2f9da4aac7ab7ca78d1a86d47162e7
dylansanjaya <dylanhikmasanjaya@gmail.com>
2023-05-09 16:45:04 +0700

    Update Footer

--- includes/footer.php
+++ includes/footer.php
@@ -5,14 +5,12 @@
         <div class="container">
             <hr>
             <ul class="list-inline pull-right">
-                <li>Fuad Muhtaz</li>
-                <li><a href="https://www.facebook.com/fuadmuhtaz" target="_blank"><img src="img/facebook.png" class="sosmed"></a></li>
-                <li><a href="https://github.com/fuadmuhtaz" target="_blank"><img src="img/github.png" class="sosmed"></a></li>
-                <li><a href="https://twitter.com/fuadmuhtaz" target="_blank"><img src="img/twitter.png" class="sosmed"></a></li>
+                <li>Dylan Hikma</li>
+                <li><a href="https://github.com/dylansanjaya" target="_blank"><img src="img/github.png" class="sosmed"></a></li>
             </ul>
             <ul class="list-inline">
-                <li>&copy; 2017</li>
-                <li>Universitas Pamulang</li>
+                <li>&copy; 2023</li>
+                <li>Universitas Negeri Semarang</li>
             </ul>
         </div>
     </footer>
