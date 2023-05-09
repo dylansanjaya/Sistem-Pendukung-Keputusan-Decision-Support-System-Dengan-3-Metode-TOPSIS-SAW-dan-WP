e6a754f59d552c0c0e011d7df93d018885f6adb9
dylansanjaya <dylanhikmasanjaya@gmail.com>
2023-05-09 16:58:03 +0700

    Update Title

--- includes/header.php
+++ includes/header.php
@@ -31,7 +31,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">SPK-TOPSIS</a>
+                <a class="navbar-brand" href="#">Sistem Pendukung Keputusan 3 Metode</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
