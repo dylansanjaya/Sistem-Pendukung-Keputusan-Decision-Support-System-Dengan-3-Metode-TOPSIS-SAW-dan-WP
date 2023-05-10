622e7f10b1227ea9fb648eca1fd3b836f218e010
dylansanjaya <dylanhikmasanjaya@gmail.com>
2023-05-10 14:05:53 +0700

    Undo Footer

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,8 @@
             <hr>
             <ul class="list-inline pull-right">
                 <li>Kelompok 9</li>
+                <li><a href="https://github.com/dylansanjaya" target="_blank"><img src="img/github.png" class="sosmed"></a></li>
+            </ul>
             <ul class="list-inline">
                 <li>&copy; 2023</li>
                 <li>Universitas Negeri Semarang</li>
