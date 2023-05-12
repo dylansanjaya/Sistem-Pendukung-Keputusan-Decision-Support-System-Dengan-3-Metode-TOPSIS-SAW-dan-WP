12c0dbd7e09fc1c0bf25282d6927abc7f2ba2625
dylansanjaya <dylanhikmasanjaya@gmail.com>
2023-05-12 21:37:00 +0700

    revert

--- matrik-r.php
+++ matrik-r.php
@@ -189,160 +189,9 @@ try {
     // edit input matrik y
 
     // edit input nilai ideal
-    if(!isset($_SESSION['matrik-r'])) {
-        header('Location: matrik-r.php');
-        exit;
-    } else {
-        $req = $dbc->prepare("SELECT * FROM pemilihan WHERE id = ?");
-        $req->bindParam(1, $_SESSION['id']);
-        $req->execute();
-    
-        $pemilihan = $req->fetch();
-    
-        $req = $dbc->prepare("SELECT * FROM matrik_y WHERE id_pemilihan = ?");
-        $req->bindParam(1, $_SESSION['id']);
-        $req->execute();
-    
-        $matrik_y = $req->fetchAll();
-    }
-    
-    $a = array();
-    
-    $tempMatrikY = array(
-        "c1" => array(),
-        "c2" => array(),
-        "c3" => array(),
-        "c4" => array(),
-        "c5" => array(),
-        "c6" => array()
-    );
-    
-    for ($i = 0; $i < count($matrik_y); $i++) {
-        $tempMatrikY['c1'][] = $matrik_y[$i]['c1'];
-        $tempMatrikY['c2'][] = $matrik_y[$i]['c2'];
-        $tempMatrikY['c3'][] = $matrik_y[$i]['c3'];
-        $tempMatrikY['c4'][] = $matrik_y[$i]['c4'];
-        $tempMatrikY['c5'][] = $matrik_y[$i]['c5'];
-        $tempMatrikY['c6'][] = $matrik_y[$i]['c6'];
-    }
-    
-    $a['positif'][] = max($tempMatrikY['c1']);
-    $a['positif'][] = max($tempMatrikY['c2']);
-    $a['positif'][] = max($tempMatrikY['c3']);
-    $a['positif'][] = min($tempMatrikY['c4']);
-    $a['positif'][] = min($tempMatrikY['c5']);
-    $a['positif'][] = min($tempMatrikY['c6']);
-    
-    $a['negatif'][] = min($tempMatrikY['c1']);
-    $a['negatif'][] = min($tempMatrikY['c2']);
-    $a['negatif'][] = min($tempMatrikY['c3']);
-    $a['negatif'][] = max($tempMatrikY['c4']);
-    $a['negatif'][] = max($tempMatrikY['c5']);
-    $a['negatif'][] = max($tempMatrikY['c6']);
-    
-    $status = "nilai-ideal";
-    try {
-        $dbc->beginTransaction();
-    
-        $dbc->exec("DELETE FROM nilai_ideal WHERE id_pemilihan = $_SESSION[id]");
-    
-        $req = $dbc->prepare("INSERT INTO nilai_ideal VALUES(:id, :ideal, :c1, :c2, :c3, :c4, :c5, :c6)");
-        $req->bindValue(':id', $_SESSION['id']);
-    
-        foreach ($a as $ideal => $y) {
-            $req->bindValue(':ideal', $ideal);
-            $req->bindValue(':c1', $y[0]);
-            $req->bindValue(':c2', $y[1]);
-            $req->bindValue(':c3', $y[2]);
-            $req->bindValue(':c4', $y[3]);
-            $req->bindValue(':c5', $y[4]);
-            $req->bindValue(':c6', $y[5]);
-            $req->execute();
-        }
-    
-        $req = $dbc->prepare("UPDATE pemilihan SET status = ? WHERE id = ?");
-        $req->bindParam(1, $status);
-        $req->bindParam(2, $_SESSION['id']);
-        $req->execute();
-    
-        $dbc->commit();
-    
-        $_SESSION['nilai-ideal'] = true;
-    } catch (PDOException $e) {
-        $dbc->rollback();
-    }
     // edit input nilai ideal
 
     // edit input jarak solusi ideal
-    if(!isset($_SESSION['matrik-r'])) {
-        header('Location: matrik-r.php');
-        exit;
-    } else {
-        $req = $dbc->prepare("SELECT * FROM pemilihan WHERE id = ?");
-        $req->bindParam(1, $_SESSION['id']);
-        $req->execute();
-    
-        $pemilihan = $req->fetch();
-    
-        $req = $dbc->prepare("SELECT * FROM alternatif WHERE id_pemilihan = ?");
-        $req->bindParam(1, $_SESSION['id']);
-        $req->execute();
-    
-        $alternatif = $req->fetchAll();
-    
-        $req = $dbc->prepare("SELECT * FROM matrik_y WHERE id_pemilihan = ?");
-        $req->bindParam(1, $_SESSION['id']);
-        $req->execute();
-    
-        $matrik_y = $req->fetchAll();
-    
-        $req = $dbc->prepare("SELECT * FROM nilai_ideal WHERE id_pemilihan = ?");
-        $req->bindParam(1, $_SESSION['id']);
-        $req->execute();
-    
-        $a = $req->fetchAll();
-        $a['positif'] = $a[0];
-        $a['negatif'] = $a[1];
-    }
-    
-    $d = array();
-    
-    foreach ($matrik_y as $nilai) {
-        $sum = pow($a['positif']['c1']-$nilai['c1'], 2) + pow($a['positif']['c2']-$nilai['c2'], 2) + pow($a['positif']['c3']-$nilai['c3'], 2)
-                + pow($a['positif']['c4']-$nilai['c4'], 2) + pow($a['positif']['c5']-$nilai['c5'], 2) + pow($a['positif']['c6']-$nilai['c6'], 2);
-        $d['positif'][] = round(sqrt($sum), 4);
-    
-        $sum = pow($nilai['c1']-$a['negatif']['c1'], 2) + pow($nilai['c2']-$a['negatif']['c2'], 2) + pow($nilai['c3']-$a['negatif']['c3'], 2)
-                + pow($nilai['c4']-$a['negatif']['c4'], 2) + pow($nilai['c5']-$a['negatif']['c5'], 2) + pow($nilai['c6']-$a['negatif']['c6'], 2);
-        $d['negatif'][] = round(sqrt($sum), 4);
-    }
-    
-    $status = "jarak-solusi-ideal";
-    try {
-        $dbc->beginTransaction();
-    
-        $dbc->exec("DELETE FROM jarak_solusi_ideal WHERE id_pemilihan = $_SESSION[id]");
-    
-        $req = $dbc->prepare("INSERT INTO jarak_solusi_ideal VALUES(:id, :positif, :negatif)");
-        $req->bindValue(':id', $_SESSION['id']);
-    
-        for ($i = 0; $i < count($alternatif); $i++) {
-            $req->bindValue(':positif', $d['positif'][$i]);
-            $req->bindValue(':negatif', $d['negatif'][$i]);
-            $req->execute();
-        }
-    
-        $req = $dbc->prepare("UPDATE pemilihan SET status = ? WHERE id = ?");
-        $req->bindParam(1, $status);
-        $req->bindParam(2, $_SESSION['id']);
-        $req->execute();
-    
-        $dbc->commit();
-    
-        $_SESSION['jarak-solusi-ideal'] = true;
-    } catch (PDOException $e) {
-        $dbc->rollback();
-    }
     // edit input jarak solusi ideal
 $page_title = 'Matrik R';
 
@@ -382,7 +231,7 @@ include './includes/header.php';
         ?>
     </table>
     <br/>
-<<<<<<< HEAD
+
     <!-- edit input matrik y!!! -->
     <h3>Normalisasi Matriks Y</h3>
     <table class="table table-bordered">
@@ -415,60 +264,10 @@ include './includes/header.php';
     <!-- edit input matrik y -->
 
     <!-- edit input nilai ideal -->
-    <h3>Nilai Ideal</h3>
-    <table class="table table-bordered">
-        <tr>
-            <th class="col-md-3">Y</th>
-            <th class="col-md-1">C1</th>
-            <th class="col-md-1">C2</th>
-            <th class="col-md-1">C3</th>
-            <th class="col-md-1">C4</th>
-            <th class="col-md-1">C5</th>
-            <th class="col-md-1">C6</th>
-        </tr>
-        <?php
-
-        foreach ($a as $ideal => $y) {
-            echo '<tr>
-                    <td class="col-md-2">'.ucfirst($ideal).'</td>
-                    <td class="col-md-1">'.$y[0].'</td>
-                    <td class="col-md-1">'.$y[1].'</td>
-                    <td class="col-md-1">'.$y[2].'</td>
-                    <td class="col-md-1">'.$y[3].'</td>
-                    <td class="col-md-1">'.$y[4].'</td>
-                    <td class="col-md-1">'.$y[5].'</td>
-                </tr>';
-        }
-        ?>
-    </table>
-    <br>
     <!-- edit input nilai ideal -->
 
     <!-- edit input solusi ideal -->
-    <h3>Jarak Solusi Ideal</h3>
-    <table class="table table-bordered">
-        <tr>
-            <th class="col-md-1">No</th>
-            <th class="col-md-5">Alternatif</th>
-            <th class="col-md-3">Positif</th>
-            <th class="col-md-3">Negatif</th>
-        </tr>
-        <?php
-
-        for ($i = 0; $i < count($alternatif); $i++) {
-            echo '<tr>
-                    <td class="col-md-1">'.($i+1).'</td>
-                    <td class="col-md-5">'.$alternatif[$i]['alternatif'].'</td>
-                    <td class="col-md-3">'.$d['positif'][$i].'</td>
-                    <td class="col-md-3">'.$d['negatif'][$i].'</td>
-                </tr>';
-        }
-        ?>
-    </table>
-    <br/>
     <!-- edit input solusi ideal -->
-=======
->>>>>>> parent of 6344ce3 (penggabungan matrik)
     <div class="row">
         <div class="col-md-6 text-left">
             <a class="btn btn-primary" href="bobot.php">&laquo; Bobot</a>
